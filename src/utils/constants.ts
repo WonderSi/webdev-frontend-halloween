e03934329418e1721fcae8b2e2b8e8586ce6fde2
WonderSi <kirillkozlov00@gmail.com>
2025-10-28 05:21:07 +0700

    feat: add constants and type index ts

---
+++ src/utils/constants.ts
@@ -0,0 +1,14 @@
+import type { TriggerWord } from '../types/index'
+
+export const TRIGGER_WORDS: readonly TriggerWord[] = [
+    'бу',
+    'тыква',
+    'призрак',
+    'мумия',
+    'ведьма',
+    'зомби',
+    'скелет',
+    'паук',
+    'череп',
+    'конфета'
+] as const
