bcfda2e7ccb5ee63255092d86680cb6fe4054b3f
WonderSi <kirillkozlov00@gmail.com>
2025-10-28 03:44:17 +0700

    feat: add element input and variables

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,28 @@
-<script>
-import AppHeader from '@/components/AppHeader.vue'
-import AppFooter from '@/components/AppFooter.vue'
-import Sidebar from '@/components/Sidebar.vue'
-
-export default {
-  name: 'App',
-  components: {
-    AppHeader,
-    AppFooter,
-    Sidebar
-  }
-}
-</script>
-
-
 <template>
-  <div id="app">
-    <AppHeader />
-    
-    <div class="container">
-      <Sidebar />
-      <main class="content">
-        <router-view />
-      </main>
-    </div>
-    
-    <AppFooter />
+  <div id="app" class="container">
+    <Input/>
   </div>
 </template>
 
+<script setup lang="ts">
+import { ref} from 'vue'
+import Input from './components/ui/TextInput.vue';
+
+const inputText = ref<string>('')
+
+</script>
+
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+@import './styles/base/_reset.scss';
+@import './styles/base/_typography.scss';
 
-#app {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-
-.container {
-  display: flex;
-  flex: 1;
-}
-
-.content {
-  flex: 1;
-  padding: 20px;
+ .container {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #222;
+    overflow: hidden;
+    position: relative;
 }
 </style>
---
+++ src/components/ui/TextInput.vue
@@ -0,0 +1,14 @@
+<template>
+    <input 
+        type="text"
+        placeholder="Привет"
+    />
+</template>
+
+<script setup lang="ts">
+
+</script>                                                                       
+
+<style scoped>
+    @import '../../styles/components/_input.scss';
+</style>
