65457ec96323ef6751e98889314980f823e97dc5
WonderSi <kirillkozlov00@gmail.com>
2025-10-29 02:11:33 +0700

    feat: add label under input

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,114 @@
 <template>
-  <div id="app" class="container">
-    <TextInput
-      v-model="inputText"
-      placeholder="Enter the word"
-    />
+    <div id="app" class="container">
+        <TextInput
+            v-model="inputText"
+            placeholder="Enter the word"
+            @keyup.enter="acceptWord"
+        />
 
-    <ScreamerBoo
-      :is-active="activeScreamer === 'бу'"
-      @close="closeScreamer"
-    />
+        <Transition name="fade">
+            <div v-if="showLabel">
+              <LabelConfimation />
+            </div>
+        </Transition>
 
-    <ScreamerPumpkin
-      :is-active="activeScreamer === 'тыква'"
-      @close="closeScreamer"
-    />
 
-    <ScreamerGhost
-      :is-active="activeScreamer === 'призрак'"
-      @close="closeScreamer"
-    /> 
+        <ScreamerBoo
+            :is-active="activeScreamer === 'бу'"
+            @close="closeScreamer"
+        />
 
-    <!--<ScreamerPumpkin
-      :is-active="activeScreamer === 'мумия'"
-      @close="closeScreamer"
-    />
+        <ScreamerPumpkin
+            :is-active="activeScreamer === 'тыква'"
+            @close="closeScreamer"
+        />
 
-    <ScreamerPumpkin
-      :is-active="activeScreamer === 'ведьма'"
-      @close="closeScreamer"
-    />
+        <ScreamerGhost
+            :is-active="activeScreamer === 'призрак'"
+            @close="closeScreamer"
+        /> 
+        <!--
+        <ScreamerMummy
+            :is-active="activeScreamer === 'мумия'"
+            @close="closeScreamer"
+        />
 
-    <ScreamerPumpkin
-      :is-active="activeScreamer === 'зомби'"
-      @close="closeScreamer"
-    />
+        <ScreamerPumpkin
+            :is-active="activeScreamer === 'ведьма'"
+            @close="closeScreamer"
+        />
 
-    <ScreamerPumpkin
-      :is-active="activeScreamer === 'скелет'"
-      @close="closeScreamer"
-    />
+        <ScreamerPumpkin
+            :is-active="activeScreamer === 'зомби'"
+            @close="closeScreamer"
+        />
 
-    <ScreamerPumpkin
-      :is-active="activeScreamer === 'паук'"
-      @close="closeScreamer"
-    />
+        <ScreamerPumpkin
+            :is-active="activeScreamer === 'скелет'"
+            @close="closeScreamer"
+        />
 
-    <ScreamerPumpkin
-      :is-active="activeScreamer === 'череп'"
-      @close="closeScreamer"
-    />
+        <ScreamerPumpkin
+            :is-active="activeScreamer === 'паук'"
+            @close="closeScreamer"
+        />
 
-    <ScreamerPumpkin
-      :is-active="activeScreamer === 'конфета'"
-      @close="closeScreamer"
-    /> -->
+        <ScreamerPumpkin
+            :is-active="activeScreamer === 'череп'"
+            @close="closeScreamer"
+        />
 
-    <ScreamerRickRoll
-      :is-active="activeScreamer === '404'"
-      @close="closeScreamer"
-    />
+        <ScreamerPumpkin
+            :is-active="activeScreamer === 'конфета'"
+            @close="closeScreamer"
+        /> -->
 
-  </div>
+        <ScreamerRickRoll
+            :is-active="activeScreamer === '404'"
+            @close="closeScreamer"
+        />
+
+    </div>
 </template>
 
 <script setup lang="ts">
-  import { ref, watch } from 'vue'
-  import TextInput from './components/ui/TextInput.vue';
-  import ScreamerBoo from './components/screamers/ScreamerBoo.vue'
-  import ScreamerPumpkin from './components/screamers/ScreamerPumpkin.vue'
-  import ScreamerGhost from './components/screamers/ScreamerGhost.vue'
-  import ScreamerRickRoll from './components/screamers/ScreamerRickRoll.vue'
-  import { TRIGGER_WORDS } from './utils/constants';
-  import type { TriggerWord } from './types/index'
+import { ref, watch } from 'vue'
+import TextInput from './components/ui/TextInput.vue';
+import LabelConfimation from './components/ui/LabelConfimation.vue';
+import ScreamerBoo from './components/screamers/ScreamerBoo.vue'
+import ScreamerPumpkin from './components/screamers/ScreamerPumpkin.vue'
+import ScreamerGhost from './components/screamers/ScreamerGhost.vue'
+import ScreamerRickRoll from './components/screamers/ScreamerRickRoll.vue'
+import { TRIGGER_WORDS } from './utils/constants';
+import type { TriggerWord } from './types/index'
 
-  const inputText = ref<string>('')
-  const activeScreamer = ref<TriggerWord | ''>('')
+const inputText = ref<string>('')
+const activeScreamer = ref<TriggerWord | ''>('')
+const showLabel = ref<boolean>(false)
 
-  const closeScreamer = ():void => {
+const closeScreamer = ():void => {
     activeScreamer.value = ''
     inputText.value = ''
-  }
+}
 
-  watch(inputText, (newValue: string) => {
-    const lowerValue = newValue.toLowerCase().trim()
-    for (const word of TRIGGER_WORDS) {
-      if (lowerValue.includes(word)) {
+const acceptWord = (): void => {
+    const word = inputText.value.toLowerCase().trim() as TriggerWord
+    if (TRIGGER_WORDS.includes(word)) {
         activeScreamer.value = word
-        // setTimeout(closeScreamer, 3000)
-        console.log(word)
-        break
-      }
+        inputText.value = ''
     }
-  })
+}
+
+watch(inputText, (newValue: string) => {
+    const lowerValue = newValue.toLowerCase().trim() as TriggerWord
+
+    if (TRIGGER_WORDS.includes(lowerValue)) {
+        showLabel.value = true
+        console.log(lowerValue)
+    } else {
+        showLabel.value = false
+    }
+})
 
 </script>
 
@@ -99,13 +116,24 @@
 @import './styles/base/_reset.scss';
 @import './styles/base/_typography.scss';
 
- .container {
+.container {
     height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
     background: #222;
     overflow: hidden;
     position: relative;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: all 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
 }
 </style>
---
+++ src/components/ui/LabelConfimation.vue
@@ -0,0 +1,16 @@
+<template>
+    <label 
+    for="main-input">
+        Press Enter
+    </label>
+</template>
+
+<script setup lang="ts">
+
+
+
+</script>
+
+<style scoped lang="scss">
+    @import '../../styles/components/_label.scss';
+</style>
